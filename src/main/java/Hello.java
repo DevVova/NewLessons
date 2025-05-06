886f8c18c29e7042dcc47fc0b583ac406cd37f79
Vladimir Zabotin <ZabotinV78@gmail.com>
2025-05-06 16:51:30 +0500

    Пробовал собрать jar и exe

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,9 @@
+import java.io.IOException;
+
 public class Hello {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World! I'm Vova. I am a trader.");
+        System.out.println("Нажми Enter для выхода...");//Это для того чтобы консоль сразу не закрывалась.
+        System.in.read(); // ждёт нажатия Enter
     }
 }
